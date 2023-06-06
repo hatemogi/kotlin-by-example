5be5d4af67fbcdab74db4af695796054fccb0764
Daehyun Kim <hatemogi@gmail.com>
2023-06-06 14:59:56 +0900

    md 소스파일 올릴 때 콘텐트타입 설정

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 ./build.sh && \
 aws --profile kotlin-book s3 cp book s3://kotlin-by-example/ --recursive --exclude '.DS_Store'
-aws --profile kotlin-book s3 cp examples s3://kotlin-by-example/src --recursive --exclude '.DS_Store'
-
+aws --profile kotlin-book s3 cp examples \
+  s3://kotlin-by-example/src --recursive --exclude '.DS_Store' \
+  --content-type="text/markdown"
